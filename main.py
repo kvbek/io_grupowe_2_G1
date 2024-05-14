4032062a79ad620961280e6aaa74c0b8d4568ae9
Natalia Jankowska <jankowska.natalia.nj@gmail.com>
2024-05-14 22:56:02 +0200

    Dodanie funkcji

--- main.py
+++ main.py
@@ -25,32 +25,13 @@ slownik = {"galeon": lista_galeon, "sykl": lista_sykl, "knut": lista_knut }
 print(slownik)
 
 def licz_sume(dict):
-    galeon_out = sum(a["galeon"]  for a in slownik)
-    sykl_out = sum(b["sykl"] for b in slownik)
-    knut_out = sum(c["knut"] for c in slownik )
-    slownik_out = {"galeon": galon_out,
+    galeon_out = sum(slownik.get("galeon")) #sum(slownik["galeon"] for item in slownik["galeon"])
+    sykl_out = sum(slownik.get("sykl")) #sum(slownik["sykl"] for item in slownik["sykl"])
+    knut_out = sum(slownik.get("knut")) #sum(slownik["knut"] for item in slownik["knut"])
+    slownik_out = {"galeon": galeon_out,
                     "sykl": sykl_out,
                     "knut": knut_out}
-    return slownik_out
+    print(slownik_out)
 
 licz_sume(slownik)
-
-
-
-# dict_in = {
-#     "galeon" : [1, 3, 5],
-#     "sykl" : [18, 20, 10],
-#     "knut" : [30, 40, 7]
-# }
-# dict_out= {}
-
-
-# def licz_sume(dict):
-# count_galon =
-# count_sykl =
-# coubt_knut =
-
-#     dict_out.keys[0] = count_galon
-#     dict_out.keys[1] = count_sykl
-#     dict_out.keys[2] = count_knut
 
